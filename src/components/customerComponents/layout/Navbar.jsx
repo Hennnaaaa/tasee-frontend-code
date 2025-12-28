2d28894e44c216b232bd272f67b7fea2717c9b74
hina <you@example.com>
2025-12-29 00:40:00 +0500

    repository checkk

--- src/components/customerComponents/layout/Navbar.jsx
+++ src/components/customerComponents/layout/Navbar.jsx
@@ -250,26 +250,7 @@ const Navbar = () => {
               )}
             </div>
 
-            {/* Search Button */}
-            <button 
-              className="text-gray-600 hover:text-blue-500"
-              aria-label="Search"
-            >
-              <svg 
-                className="w-6 h-6" 
-                fill="none" 
-                stroke="currentColor" 
-                viewBox="0 0 24 24" 
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </button>
+            
 
             {/* Wishlist Heart Icon */}
             {isAuthenticated && (
