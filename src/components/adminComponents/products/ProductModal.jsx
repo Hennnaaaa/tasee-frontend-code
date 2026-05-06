e0154704e7b5e27369e28665bc282cd79b7fbb84
hina <you@example.com>
2026-05-06 16:14:52 +0500

    primary image displays at first

--- src/components/adminComponents/products/ProductModal.jsx
+++ src/components/adminComponents/products/ProductModal.jsx
@@ -210,7 +210,7 @@ const ImageUpload = ({ onImagesChange, maxImages = 15, existingImages = [], onRe
           htmlFor="image-upload"
           className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 cursor-pointer"
         >
-          Upload Images Range:  ({previews.length}/{maxImages})
+          Upload Images ({previews.length}/{maxImages})
         </label>
         
         <span className="text-sm text-gray-500">
